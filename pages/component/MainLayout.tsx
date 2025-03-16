9e6310d5066a0b02123fd00d0a61b7b2cab9d26e
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-03-16 21:41:15 +0100

    fix

--- pages/component/MainLayout.tsx
+++ pages/component/MainLayout.tsx
@@ -4,19 +4,31 @@ import Sidebar from "./sidebar";
 import React from "react";
 
 interface MainLayoutProps {
-  children: ReactNode;
-}
+    children: ReactNode;
+  }
+  
+  interface ChildProps {
+    isSidebarOpen: boolean;
+  }
 
 export default function MainLayout({ children }: MainLayoutProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   // Pass isSidebarOpen to children
+//   const childrenWithProps = React.Children.map(children, (child) => {
+//     if (React.isValidElement(child)) {
+//       return React.cloneElement(child, { isSidebarOpen });
+//     }
+//     return child;
+//   });
+  // Pass isSidebarOpen to children
   const childrenWithProps = React.Children.map(children, (child) => {
-    if (React.isValidElement(child)) {
+    if (React.isValidElement<ChildProps>(child)) {
       return React.cloneElement(child, { isSidebarOpen });
     }
     return child;
   });
+
 
   return (
     <div className="bg-black">
